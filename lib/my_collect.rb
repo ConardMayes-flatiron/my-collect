e79fb720f908b25bf158822511b4a4ccfdfc2eda
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-21 21:32:39 -0600

    commit tree 9023fd319d58e44a0dc826280f7e6ab92663ae7d

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
 
+def my_collect(array)
+    if array.length > 0
+        i = 0
+        collection = []
+        orig_array = []
+
+        while i < array.length
+            collection << yield(array[i])
+            i+=1
+        end
+        puts orig_array
+
+        collection
+
+    else
+        puts "That's an empty array, yo."
+    end
+end
